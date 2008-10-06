b50d103edca8ad5bffa4eef1f151c2fd8ddf0563
Roland Swingler <roland.swingler@gmail.com>
2008-10-06 20:42:40 +0100

    Fixing migration names

--- generators/trivial_tokens/trivial_tokens_generator.rb
+++ generators/trivial_tokens/trivial_tokens_generator.rb
@@ -4,7 +4,7 @@ class TrivialTokensGenerator < Rails::Generator::NamedBase
       m.class_collisions( [:Token] )
       m.directory "app/models"
       m.template "token.erb", "app/models/token.rb"
-      m.migration_template "token_migration.erb", "db/migrate"
+      m.migration_template "migration.erb", "db/migrate", :migration_file_name => "create_tokens"
     end
   end
 end
