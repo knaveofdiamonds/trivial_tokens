9aed6cd9d35c2bb5945b06b585f3a8f65b50283c
Roland Swingler <roland.swingler@gmail.com>
2008-10-07 19:13:47 +0100

    Adding in a Users Controller, splitting out manifest into template method to make it easier to subclass

--- generators/trivial_tokens/trivial_tokens_generator.rb
+++ generators/trivial_tokens/trivial_tokens_generator.rb
@@ -1,17 +1,40 @@
 class TrivialTokensGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
-      m.class_collisions( [:Token, class_name] )
-
-      m.directory "app/models"
-      m.directory "app/lib"
-
-      m.template "login.erb", "lib/trivial_login.rb"
-      m.template "token.erb", "app/models/token.rb"
-      m.template "user.erb", "app/models/#{singular_name}.rb"
-
-      m.migration_template "migration.erb", "db/migrate", :migration_file_name => "create_tokens"
-      m.migration_template "users.erb", "db/migrate", :migration_file_name => "create_#{table_name}"
+      check_for_class_collisions m
+      create_directories m
+      copy_templates m
+      copy_migration_templates m
+      add_routes m
     end
+  end
+
+  protected
+
+  def check_for_class_collisions(m)
+    m.class_collisions( [:Token, class_name] )
+  end
+
+  def create_directories(m)
+    m.directory "app/models"
+    m.directory "app/controllers"
+    m.directory "app/views/#{plural_name}"
+    m.directory "app/lib"
+  end
+
+  def copy_templates(m)
+    m.template "login.erb", "lib/trivial_login.rb"
+    m.template "token.erb", "app/models/token.rb"
+    m.template "user.erb", "app/models/#{singular_name}.rb"
+    m.template "users_controller.erb", "app/controllers/#{plural_name}_controller.rb"
+  end
+
+  def copy_migration_templates(m)
+    m.migration_template "token_migration.erb", "db/migrate", :migration_file_name => "create_tokens"
+    m.migration_template "user_migration.erb", "db/migrate", :migration_file_name => "create_#{table_name}"
+  end
+
+  def add_routes(m)
+    m.route_resources plural_name
   end
 end
