4afd02e267c8c2ef2ef107fb651aab685a033d87
Roland Swingler <roland.swingler@gmail.com>
2008-10-07 18:41:55 +0100

    Start of simple signup generator

--- generators/trivial_tokens/trivial_tokens_generator.rb
+++ generators/trivial_tokens/trivial_tokens_generator.rb
@@ -1,10 +1,17 @@
 class TrivialTokensGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
-      m.class_collisions( [:Token] )
+      m.class_collisions( [:Token, class_name] )
+
       m.directory "app/models"
+      m.directory "app/lib"
+
+      m.template "login.erb", "lib/trivial_login.rb"
       m.template "token.erb", "app/models/token.rb"
+      m.template "user.erb", "app/models/#{singular_name}.rb"
+
       m.migration_template "migration.erb", "db/migrate", :migration_file_name => "create_tokens"
+      m.migration_template "users.erb", "db/migrate", :migration_file_name => "create_#{table_name}"
     end
   end
 end
